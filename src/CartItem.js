8e0c08d235a1b473a426eae70d24ba78606422d7
Yash-Kr <yashk.ug19.cs@nitp.ac.in>
2021-02-23 23:31:06 +0530

    Added Functionality for (+)Plus Button

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,6 +12,11 @@ class CartItem extends React.Component {
             img: ''
         }
     }
+    increaseQty = () =>
+    {
+        console.log("this : ",this.state);
+    }
+   
     render(){
         const { price, title, qty } = this.state;
     return(
@@ -24,7 +29,11 @@ class CartItem extends React.Component {
                 <div style={{color : '#777'}}>Rs {price}</div>
                 <div style={{color : '#777'}}>Qty: {qty}</div>
                 <div className = "cart-item-actions">
-                    <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1237/1237946.svg?token=exp=1614101743~hmac=43ac88ebd61b9dceb198dcb7f2f91efd"/>
+                    <img alt="increase" 
+                         className="action-icons" 
+                         src="https://www.flaticon.com/svg/vstatic/svg/1237/1237946.svg?token=exp=1614101743~hmac=43ac88ebd61b9dceb198dcb7f2f91efd"
+                         onClick = {this.increaseQty}   
+                         />
                     <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/56/56889.svg?token=exp=1614101782~hmac=54eac46771961d4bfcd4ed754e5c7d6c"/>
                     <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/3096/3096687.svg?token=exp=1614101804~hmac=9200ed21528518eb8ac97a2e3267d7fb"/>
                 </div>
